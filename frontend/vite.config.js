508e6de619730a84ad49b96947c15b293c67b549
mohanrajnagooran <77579188+mohanrajnagooran@users.noreply.github.com>
2026-02-02 08:48:26 +0530

    frontend code changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(),react()],
     server: {
     port: 5173,               // ensure Electron loads this
     strictPort: true,         // fail if 5173 is not available
